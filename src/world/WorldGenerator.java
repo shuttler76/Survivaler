428f57d3252e205e996c4f8ca9d8472bc9c8cb64
shuttler67 <shuttler67@gmail.com>
2015-03-01 14:47:30 +0100

    Trying out perlin noise for world gen

--- src/world/WorldGenerator.java
+++ src/world/WorldGenerator.java
@@ -2,18 +2,50 @@ package world;
 
 import util.SimplexNoise;
 
-//TODO Pass in world dimensions in generate()
-
 public class WorldGenerator {
 
 	public static int[][] generate(int worldsize){
 		int[][] map = new int[worldsize][worldsize];
-		for(int x=0;x<worldsize;x++){
-			for(int y=0;y<worldsize;y++){
-				double typeValue = Math.abs(SimplexNoise.noise((double) x / 250, (double) y / 250, 0));
-				map[x][y]= (int) (typeValue * 5);
-			}
-		}
+
+        float[][] totalNoise = new float[worldsize][worldsize];
+        int octaves = 3;
+        float roughness = 0.2f;
+        float layerFrequency = 0.005f;
+        float layerWeight = 1;
+        float weightSum = 0;
+
+        for (int octave = 0; octave < octaves; octave++) {
+            for (int x = 0; x < worldsize; x++) {
+                for (int y = 0; y < worldsize; y++) {
+                    totalNoise[x][y] += (float)SimplexNoise.noise((double) x * layerFrequency, (double) y * layerFrequency, layerWeight);
+                }
+            }
+
+            layerFrequency *= 2;
+            weightSum += layerWeight;
+            layerWeight *= roughness;
+        }
+
+        for (int x = 0; x < worldsize; x++) {
+            for (int y = 0; y < worldsize; y++) {
+
+                if ((totalNoise[x][y] < -0.4)) {
+                    map[x][y] = CubeType.DEEPWATER;
+                }
+                else if (totalNoise[x][y] < 0) {
+                    map[x][y] = CubeType.WATER;
+                }
+                else if ((totalNoise[x][y] < 0.1)) {
+                    map[x][y] = CubeType.SAND;
+                }
+                else if ((totalNoise[x][y] < 1)) {
+                    map[x][y] = CubeType.GRASS;
+                }
+                else {
+                    map[x][y] = CubeType.STONE;
+                }
+            }
+        }
 		return map;
 	}
 }
